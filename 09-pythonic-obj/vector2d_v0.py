325e4c20e5e7db6697052fb67b6d331e256552a0
辛海波 <xinhb@vastio.com>
2020-08-11 15:57:05 +0800

    update

---
+++ 09-pythonic-obj/vector2d_v0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/11 15:10
+# @Author  : xhb
+# @FileName: vector2d_v0.py
+# @Software: PyCharm
+
+
+from array import array
+import math
+
+
+class Vector2d:
+    typecode = 'd'  # <1>
+    
--- test_letcode.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from letcode import *
-
-
-@pytest.mark.parametrize('nums, target, result', [([1,1,2],2,[0,1]), ([-1,-2,4],2,[1,2])])
-def test_two_sum(nums, target, result):
-    assert two_sum(nums, target) == result
